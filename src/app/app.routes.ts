e5a94ba380da5395ed55d3bc898ea37f7b243dd4
michael <micjejim@espol.edu.ec>
2025-01-25 17:09:26 -0500

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importa el componente Home
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta raíz para Home
+];
