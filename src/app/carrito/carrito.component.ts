379a5ce6ffda346f1ead06cf26a5e979f79c9a43
paulethTandazo <117050687+paulethTandazo@users.noreply.github.com>
2025-01-29 22:38:10 -0500

    agregando metodos par el carrito de compras

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -23,4 +23,25 @@ export class CarritoComponent implements OnInit {
     this.cartItems = this.cartItems.filter(item => item.id !== productId); // Eliminar producto
     localStorage.setItem('cart', JSON.stringify(this.cartItems)); // Guardar el carrito actualizado
   }
+  shippingCost: number = 0; // dejaremos por defecto envío gratuito
+  getTotalItems(): number {
+    return this.cartItems.reduce((total, product) => total + product.quantity, 0);
+  }
+  getSubtotal(): number {
+    return this.cartItems.reduce((subtotal, product) => subtotal + (product.price * product.quantity), 0);
+  }
+  getTax(): number {
+    return this.getSubtotal() * 0.15;
+  }
+  getTotal(): number {
+    return this.getSubtotal() + this.getTax() + this.shippingCost;
+  }
+  setShippingCost(cost: number): void {
+    this.shippingCost = cost;
+  }
+  updateCart(): void {
+    console.log('Carrito actualizado:', this.cartItems);
+  }
+
+
 }
